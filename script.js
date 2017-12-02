3998229cca114e48771b418696e85ece6e0fba30
Dan Bateman <dbateman0319@starkstate.net>
2017-12-01 23:59:23 -0500

    Successfull form validation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let path = require('path');
+let expressValidator = require('express-validator');
 
 let app = express();
 
@@ -23,6 +24,30 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// global variables
+app.use(function(req, res, next) {
+	res.locals.errors = null;
+	next();
+});
+
+// Express validator middleware
+app.use(expressValidator({
+	errorFormatter: function(param, msg, value) {
+		var namespace = param.split('.'),
+		root = namespace.shift(),
+		formParam = root;
+
+		while(namespace.length) {
+			formParam += '[' + namespace.shift() + ']';
+		}
+		return {
+			param: formParam,
+			msg: msg,
+			value: value
+		};
+	}
+})); 
+
 let users = [
 	{
 		first_name: 'Dan',
@@ -42,10 +67,27 @@ app.get('/', function(req, res) {
 });
 
 app.post('/users/add', function(req, res) {
-	let newUser = {
-		first_name: req.body.first_name;
-		last_name: req.body.last_name;
-	}
+	
+	req.checkBody('first_name', 'first name is required').notEmpty();
+	req.checkBody('last_name', 'Last name is required').notEmpty();
+	
+	let errors = req.validationErrors();
+	
+	if(errors){
+		console.log('Error');
+		res.render('index', {
+			title: 'Hello there buddy',
+			users: users,
+			errors: errors,
+	});
+	} else {
+		let newUser = {
+			first_name: req.body.first_name,
+			last_name: req.body.last_name,
+		};
+
+		console.log('success');
+	} 
 	
 });
 
